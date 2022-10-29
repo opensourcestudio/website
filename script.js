135334d30c63d2f752560cc87c1b75fd027577b0
Tawseef-Reza <tawseefrezahome@gmail.com>
2022-10-29 13:15:56 -0400

    Fixed Sizing based on Different Window Sizes

--- script.js
+++ script.js
@@ -1,12 +1,63 @@
+let isOut = false;
+let height_list = 0;
 function loader() {
+    checkSize()
     setTimeout(() => {document.querySelector("#h1time").style.opacity = 1;}, 1300)
 
 }
+const checkSize  = () => {
+    console.log("this ran");
+    if (window.innerWidth <= 880) {
+        document.querySelector("#h1time h6").style.display="none";
+        document.querySelector("#h1time h1").innerHTML = "OSS";
+    }
+    else {
+        document.querySelector("#h1time h6").style.display="block";
+        document.querySelector("#h1time h1").innerHTML = "The Open Source Studio";
+    }
+}
+window.addEventListener('resize', checkSize)
 
-window.addEventListener("resize", () => {
-    var about = document.querySelector("#aboutEl").getBoundingClientRect()
-    var studio = document.querySelector("#aboutEl").getBoundingClientRect()
-    console.log(`${window.innerWidth}, ${window.innerHeight}`);
-    // if (about.left < 1200)
-    console.log(`studio is ${studio.left}left ${studio.right}right, about is ${about.left}left ${about.right}right`);
-})
+function work() {
+    
+    if (!isOut) {
+        console.log("firstran")
+        document.querySelector("#dropbtn").setAttribute("disabled", "")
+        
+        document.querySelector("#dropbtn").style.border = "3px solid rgb(116, 156, 221)"
+    var list_expand = setInterval(() => {
+        document.querySelector("#dropdown").style.height = `${height_list}px`
+        height_list += 10
+        if (height_list == 220) {
+            document.querySelector("#dropdown").style.height = `${height_list}px`
+            document.querySelector("#dropbtn").removeAttribute("disabled")
+            clearInterval(list_expand)
+        }
+    }, 10)
+    
+    
+    isOut = true
+    
+    }
+    else {
+        console.log("secondran")
+        document.querySelector("#dropbtn").setAttribute("disabled", "")
+        
+        document.querySelector("#dropbtn").style.border = "1px solid rgb(116, 156, 221)"
+        var list_decrease = setInterval(() => {
+        document.querySelector("#dropdown").style.height = `${height_list}px`
+        height_list -= 10
+        if (height_list == 0) {
+            document.querySelector("#dropdown").style.height = `${height_list}px`
+            document.querySelector("#dropbtn").removeAttribute("disabled")
+            clearInterval(list_decrease)
+        }
+    }, 10)
+    
+
+    isOut = false
+    
+        
+    }
+}
+
