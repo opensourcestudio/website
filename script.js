bf0ae6b215257709d019f08fc5153733a57c585e
Naowal Rahman <naowal.ar@gmail.com>
2022-12-27 13:16:27 -0500

    ES6 GLTFLoader migration + JS formatting

--- script.js
+++ script.js
@@ -1,63 +1,65 @@
+// responsive drop down menu
+
 let isOut = false;
 let height_list = 0;
 function loader() {
     checkSize()
-    setTimeout(() => {document.querySelector("#h1time").style.opacity = 1;}, 1300)
+    setTimeout(() => { document.querySelector("#h1time").style.opacity = 1; }, 1300)
 
 }
-const checkSize  = () => {
+const checkSize = () => {
     console.log("this ran");
     if (window.innerWidth <= 880) {
-        document.querySelector("#h1time h6").style.display="none";
+        document.querySelector("#h1time h6").style.display = "none";
         document.querySelector("#h1time h1").innerHTML = "OSS";
     }
     else {
-        document.querySelector("#h1time h6").style.display="block";
+        document.querySelector("#h1time h6").style.display = "block";
         document.querySelector("#h1time h1").innerHTML = "The Open Source Studio";
     }
 }
 window.addEventListener('resize', checkSize)
 
 function work() {
-    
+
     if (!isOut) {
         console.log("firstran")
         document.querySelector("#dropbtn").setAttribute("disabled", "")
-        
+
         document.querySelector("#dropbtn").style.border = "3px solid #8440ae"
-    var list_expand = setInterval(() => {
-        document.querySelector("#dropdown").style.height = `${height_list}px`
-        height_list += 10
-        if (height_list == 220) {
+        var list_expand = setInterval(() => {
             document.querySelector("#dropdown").style.height = `${height_list}px`
-            document.querySelector("#dropbtn").removeAttribute("disabled")
-            clearInterval(list_expand)
-        }
-    }, 10)
-    
-    
-    isOut = true
-    
+            height_list += 10
+            if (height_list == 220) {
+                document.querySelector("#dropdown").style.height = `${height_list}px`
+                document.querySelector("#dropbtn").removeAttribute("disabled")
+                clearInterval(list_expand)
+            }
+        }, 10)
+
+
+        isOut = true
+
     }
     else {
         console.log("secondran")
         document.querySelector("#dropbtn").setAttribute("disabled", "")
-        
+
         document.querySelector("#dropbtn").style.border = "1px solid #8440ae"
         var list_decrease = setInterval(() => {
-        document.querySelector("#dropdown").style.height = `${height_list}px`
-        height_list -= 10
-        if (height_list == 0) {
             document.querySelector("#dropdown").style.height = `${height_list}px`
-            document.querySelector("#dropbtn").removeAttribute("disabled")
-            clearInterval(list_decrease)
-        }
-    }, 10)
-    
+            height_list -= 10
+            if (height_list == 0) {
+                document.querySelector("#dropdown").style.height = `${height_list}px`
+                document.querySelector("#dropbtn").removeAttribute("disabled")
+                clearInterval(list_decrease)
+            }
+        }, 10)
 
-    isOut = false
-    
-        
+
+        isOut = false
+
+
     }
 }
 
--- threeD.js
+++ threeD.js
@@ -1,14 +1,17 @@
+import * as THREE from 'three';
+import { GLTFLoader } from 'GLTFLoader'
+
 const inw = window.innerWidth - (window.innerWidth * 0.015);
 const pi = Math.PI;
 const inh = window.innerHeight;
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(100, window.innerWidth/window.innerHeight,0.1,1000);
-const renderer = new THREE.WebGLRenderer({antialias: true});
+const camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer({ antialias: true });
 const canvas = document.getElementById("ctx");
 // var raycaster = new THREE.Raycaster();
 var mouse = new THREE.Vector2();
 var geometry = new THREE.BoxGeometry(0.02, 0.02, 1.2);
-var material = new THREE.MeshLambertMaterial({color: 0xF7F7F7});
+var material = new THREE.MeshLambertMaterial({ color: 0xF7F7F7 });
 let Mesh;
 function clamp(number, min, max) {
     return Math.max(min, Math.min(number, max));
@@ -20,80 +23,77 @@ function rnr(min, max) {
 
 function init() {
     renderer.setClearColor("#121212");
-renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.z = 5;
-canvas.appendChild(renderer.domElement);
-window.addEventListener('resize', () => {
-renderer.setSize(window.innerWidth, window.innerHeight);
-camera.aspect = window.innerWidth / window.innerHeight;
-camera.updateProjectionMatrix();
-})
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.position.z = 5;
+    canvas.appendChild(renderer.domElement);
+    window.addEventListener('resize', () => {
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+    })
 }
 
 
 
 
 
-let balloonLoader = new THREE.GLTFLoader();
+let balloonLoader = new GLTFLoader();
 
-        balloonLoader.load('models/osslogo.gltf', (gltf) => {
-            Mesh = gltf.scene;
-            let smth = 0.5;
-            Mesh.scale.set(smth, smth, smth);
-            scene.add(Mesh);
-            Mesh.position.x = 0;
-            Mesh.position.y = 0;
-            Mesh.position.z = -10;
-            Mesh.rotation.set(pi/2, pi, 0);
-            Mesh.name = "oss";
-            // console.log(Mesh);
-        });
+balloonLoader.load('models/osslogo.gltf', (gltf) => {
+    Mesh = gltf.scene;
+    let smth = 0.5;
+    Mesh.scale.set(smth, smth, smth);
+    scene.add(Mesh);
+    Mesh.position.x = 0;
+    Mesh.position.y = 0;
+    Mesh.position.z = -10;
+    Mesh.rotation.set(pi / 2, pi, 0);
+    Mesh.name = "oss";
+    // console.log(Mesh);
+});
+
 function makeit() {
     // const controls = new OrbitControls(camera, renderer.domElement);
-    
-        
-for (var i = 0; i<90;i++ ){
-    
-    var mesh = new THREE.Mesh(geometry, material);
-    var threechoice = Math.random();
-    
-    if (threechoice >= 0 && threechoice < 1/3) 
-    {
-        
-        var first = rnr(0.2, 0.5);
-        mesh.position.x = first * 15;
-        mesh.position.y = (Math.random()-0.5) * 6;
-        mesh.position.z = (Math.random() - 0.3) * 15;
-        
-    } 
-    else if (threechoice >= 1/3 && threechoice < 2/3) 
-    {
-        
-        var second = rnr(-0.5, -0.2);
-        mesh.position.x = second * 15;
-        mesh.position.y = (Math.random()-0.5) * 6;
-        mesh.position.z = (Math.random() - 0.3) * 15;
-    } 
-    else 
-    {
-        
-        var third = rnr(-0.2, 0.2);
-        mesh.position.x = third * 14;
-        if (Math.round(Math.random()) == 1) {
-            mesh.position.y = rnr(0.2, 0.5) * 14;
-            // console.log("first");
+
+    for (var i = 0; i < 90; i++) {
+
+        var mesh = new THREE.Mesh(geometry, material);
+        var threechoice = Math.random();
+
+        if (threechoice >= 0 && threechoice < 1 / 3) {
+
+            var first = rnr(0.2, 0.5);
+            mesh.position.x = first * 15;
+            mesh.position.y = (Math.random() - 0.5) * 6;
+            mesh.position.z = (Math.random() - 0.3) * 15;
+
+        }
+        else if (threechoice >= 1 / 3 && threechoice < 2 / 3) {
+
+            var second = rnr(-0.5, -0.2);
+            mesh.position.x = second * 15;
+            mesh.position.y = (Math.random() - 0.5) * 6;
+            mesh.position.z = (Math.random() - 0.3) * 15;
         }
         else {
-            mesh.position.y = rnr(-0.5, -0.2) * 14;
-            // console.log("second");
+
+            var third = rnr(-0.2, 0.2);
+            mesh.position.x = third * 14;
+            if (Math.round(Math.random()) == 1) {
+                mesh.position.y = rnr(0.2, 0.5) * 14;
+                // console.log("first");
+            }
+            else {
+                mesh.position.y = rnr(-0.5, -0.2) * 14;
+                // console.log("second");
+            }
+            mesh.position.z = (Math.random() - 0.3) * 15;
+
+
         }
-        mesh.position.z = (Math.random() - 0.3) * 15;
-        
-    
+        mesh.name = "bob";
+        scene.add(mesh);
     }
-    mesh.name = "bob";
-    scene.add(mesh);
-}
 
 }
 
@@ -135,12 +135,12 @@ function setLight() {
 // }
 function animate() {
     requestAnimationFrame(animate);
-    Mesh.rotation.set(Mesh.rotation.x +0.01, 
-                      Mesh.rotation.y +0.015,
-                      Mesh.rotation.z +0.01);
+    Mesh.rotation.set(Mesh.rotation.x + 0.01,
+        Mesh.rotation.y + 0.015,
+        Mesh.rotation.z + 0.01);
     scene.traverse(function (object) {
         if (object.isMesh && object.name == "bob") {
-            
+
             if (object.position.z <= -11) {
                 object.position.z = 10;
             }
@@ -152,7 +152,7 @@ function animate() {
 
         }
     })
-    
+
     renderer.render(scene, camera);
 }
 
